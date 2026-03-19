410dc20a51ea32141bcd404e2e58868ffe41187d
sachin <sachinesky@gmail.com>
2026-03-19 21:09:15 +0530

    fixed error

--- app_ml.py
+++ app_ml.py
@@ -41,6 +41,12 @@ st.write(percentage)
 X = df.drop('target', axis=1)
 y = df['target']
 
+# Convert text to number
+X = pd.get_dummies(X)
+
+# Fill missing values
+X = X.fillna(0)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # ------------------------------
